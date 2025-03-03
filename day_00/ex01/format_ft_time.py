6237d289d1c38b0ce3ee4e38a850189bde2bd3b9
clemllovio <llovio.clemence@gmail.com>
2025-03-03 20:24:26 +0100

    ex01: time at the good format

--- day_00/ex01/format_ft_time.py
+++ day_00/ex01/format_ft_time.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
 dt = datetime.today()
+
 seconds = datetime.timestamp(dt)
-scientific_format = "{:e}".format(seconds)
+formated_seconds = f"{seconds:,.4f}"
+
+scientific_format = "{:.2e}".format(seconds)
+
 date_str = dt.strftime("%b %d %Y")
 
-print("Seconds since January 1, 1970:", seconds, "or", scientific_format, "in scientific format")
+print("Seconds since January 1, 1970:", formated_seconds, "or", scientific_format, "in scientific format")
 print(date_str)
